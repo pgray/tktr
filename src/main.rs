3b470a72e04722623810182ab4d667f928c38ae7
pgray <pgray@ns1.com>
2019-05-25 14:52:03 -0700

    add in tui example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,143 @@
-use cursive::views::TextView;
-use cursive::Cursive;
+#[allow(dead_code)]
+mod util;
+use std::io;
 
-fn main() {
-    let mut siv = Cursive::default();
-    siv.add_global_callback('q', |s| s.quit());
-    siv.add_layer(TextView::new("Hello cursive"));
-    siv.run();
+extern crate termion;
+
+use termion::event::Key;
+use termion::input::MouseTerminal;
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Corner, Direction, Layout};
+use tui::style::{Color, Modifier, Style};
+use tui::widgets::{Block, Borders, List, SelectableList, Text, Widget};
+use tui::Terminal;
+
+use crate::util::event::{Event, Events};
+
+struct App<'a> {
+    items: Vec<&'a str>,
+    selected: Option<usize>,
+    events: Vec<(&'a str, &'a str)>,
+    info_style: Style,
+    warning_style: Style,
+    error_style: Style,
+    critical_style: Style,
+}
+
+impl<'a> App<'a> {
+    fn new() -> App<'a> {
+        App {
+            items: vec![
+                "item1", "item2", "item3", "item1", "item2", "item3", "item1", "item2", "item3",
+            ],
+            selected: None,
+            events: vec![
+                ("Event1", "INFO"),
+                ("Event2", "INFO"),
+                ("Event3", "CRITICAL"),
+                ("Event4", "ERROR"),
+                ("Event5", "INFO"),
+                ("Event6", "INFO"),
+                ("Event7", "WARNING"),
+                ("Event8", "INFO"),
+                ("Event9", "INFO"),
+            ],
+            info_style: Style::default().fg(Color::White),
+            warning_style: Style::default().fg(Color::Yellow),
+            error_style: Style::default().fg(Color::Magenta),
+            critical_style: Style::default().fg(Color::Red),
+        }
+    }
+    fn advance(&mut self) {
+        let event = self.events.pop().unwrap();
+        self.events.insert(0, event);
+    }
+}
+
+fn main() -> Result<(), failure::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    let events = Events::new();
+
+    let mut app = App::new();
+    loop {
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                .split(f.size());
+            let style = Style::default().fg(Color::Black).bg(Color::White);
+            SelectableList::default()
+                .block(Block::default().borders(Borders::ALL).title("List"))
+                .items(&app.items)
+                .select(app.selected)
+                .style(style)
+                .highlight_style(style.fg(Color::LightGreen).modifier(Modifier::BOLD))
+                .highlight_symbol(">")
+                .render(&mut f, chunks[0]);
+            {
+                let events = app.events.iter().map(|&(evt, level)| {
+                    Text::styled(
+                        format!("{}: {}", level, evt),
+                        match level {
+                            "ERROR" => app.error_style,
+                            "CRITICAL" => app.critical_style,
+                            "WARNING" => app.warning_style,
+                            _ => app.info_style,
+                        },
+                    )
+                });
+                List::new(events)
+                    .block(Block::default().borders(Borders::ALL).title("List"))
+                    .start_corner(Corner::BottomLeft)
+                    .render(&mut f, chunks[1]);
+            }
+        })?;
+
+        match events.next()? {
+            Event::Input(input) => match input {
+                Key::Char('q') => {
+                    break;
+                }
+                Key::Left => {
+                    app.selected = None;
+                }
+                Key::Down => {
+                    app.selected = if let Some(selected) = app.selected {
+                        if selected >= app.items.len() - 1 {
+                            Some(0)
+                        } else {
+                            Some(selected + 1)
+                        }
+                    } else {
+                        Some(0)
+                    }
+                }
+                Key::Up => {
+                    app.selected = if let Some(selected) = app.selected {
+                        if selected > 0 {
+                            Some(selected - 1)
+                        } else {
+                            Some(app.items.len() - 1)
+                        }
+                    } else {
+                        Some(0)
+                    }
+                }
+                _ => {}
+            },
+            Event::Tick => {
+                app.advance();
+            }
+        }
+    }
+
+    Ok(())
 }
